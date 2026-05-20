4c4bba9d73d0c09638d84c249dc687511d945f3a
Jjpablo28 <jpr.rod06@gmail.com>
2026-05-20 00:44:06 -0500

    Init front

--- agendamientoCitasMongo/src/app/components/pacientes/pacientes.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pacientes',
-  imports: [],
-  templateUrl: './pacientes.html',
-  styleUrl: './pacientes.css',
-})
-export class Pacientes {}
